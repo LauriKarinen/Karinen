13b54f9a7d662dca172ac75c37f34c4e4a7bf598
LauriKarinen <56953837+LauriKarinen@users.noreply.github.com>
2019-11-19 10:09:04 +0200

    Create aivot.js

---
+++ eduvantaa-master/aivot.js
@@ -0,0 +1,35 @@
+function laatikkoFunktio(boxNumber){
+    alert("Nyt vaihdetaan laatikko"+ boxNumber + ":n sisältöä!");
+
+    if (boxNumber == 1){
+        document.getElementById("laatikko1").innerHTML = "<h1 class='uusiOtsikko1'>Tähän tulee uusi otsikko!</h1>" + 
+        "<p>Ja tähän tietysti jotain sisältöä...</p>";
+        document.getElementById("laatikko1").style.lineHeight = "50px";
+    }
+    else if (boxNumber == 2){
+        document.getElementById("laatikko2").innerHTML = "<Table>"+
+        "<tr> <td> Ensimmäinen sisältö </td> </tr> </Table>";
+        
+    }
+        // toiminnallisuus puuttuu!
+    
+    else if (boxNumber == 3){
+        document.getElementById("laatikko3").innerHTML = "<img src='smiley1.png'>";
+        //toiminnallisuus puuttuu!
+
+    }
+    else if (boxNumber == 4){
+        document.location.reload();
+    }
+}
+function yhteystietoFunktio() {
+    var x = document.getElementById("yhteystietolomake");
+    var text = "";
+    var i;
+    for (i = 1; i < x.length; i++) {
+      text += x.elements[i].value + "<br>";
+    }
+    document.getElementById("yhteystiedotTuloste").innerHTML = text;
+  }
+
+
